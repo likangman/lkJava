21bff441b669505c052a1b83daca962df59c6b9d
likangman <171288260@qq.com>
2018-05-15 21:49:01 +0800

    #2#~O_O~

--- src/me/abcabc/java_io/Java_io.java
+++ src/me/abcabc/java_io/Java_io.java
@@ -1,9 +1,46 @@
 package me.abcabc.java_io;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.Test;
 public class Java_io {
-	public static void main(String[] argus)
+	
+	/**
+	 * @throws IOException *
+	 * 
+	 * 
+	 * 
+	 */
+	@Test
+	public void test() throws IOException
 	{
 		
+		InputStream in = new FileInputStream("./././others/test_01.txt");
+		byte[] bt = new byte[100];
+		int btCount;
+		while((btCount=in.read(bt))!=-1)
+		{
+			String s = new String(bt); 
+			System.out.println(s);
+			for(byte b:bt)
+			{
+				System.out.print("-"+(char)b+b);
+			}
+		}
+	}
+	
+	public void test2()
+	{
+		System.out.println(Byte.MAX_VALUE);
 	}
 
 }
+/**
+*IO流常用的基类：InputStream  OutputStream
+*字符流的抽象基类：Reader  Writer
+*
+*
+*
+*/
